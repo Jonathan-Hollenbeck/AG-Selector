6701f20d179c357b268dc3db0903eb871f4c7848
jonny <jonikeule@web.de>
2024-08-17 00:20:22 +0200

    fixed select preferences

--- lib/view/select_preferences.dart
+++ lib/view/select_preferences.dart
@@ -29,56 +29,77 @@ class SelectPreferences extends StatefulWidget {
 
 class _SelectPreferencesState extends State<SelectPreferences> {
   List<String> numberOfPreferencesList = [];
-
   List<PersonAgPreference> personAgPreferences = [];
+
+  Map<int, Map<String, String>> agNWeekdayToPreference =
+      <int, Map<String, String>>{};
 
   Settings settings = Settings(Settings.defaultNumberOfPreferences);
 
-  void setAGPreference(AG ag, String preferenceString) {
+  void setAGPreference(AG ag, String weekday, String preferenceString) {
     setState(() {
-      PersonAgPreference? personAgPreference = getPersonAgPreferenceFromAG(ag);
-      if (personAgPreference != null && preferenceString != "") {
+      if (preferenceString != "") {
+        //get int from preference String
         int preference = int.parse(preferenceString);
 
-        personAgPreference.preferenceNumber = preference;
+        //reset previouse agNWeekdayToPreference for View
+        PersonAgPreference? personAgPreferenceToReset =
+            getPersonAgPreferenceByWeekdayAndPreferenceNumber(
+                weekday, preference);
+        if (personAgPreferenceToReset != null &&
+            agNWeekdayToPreference[personAgPreferenceToReset.ag.id] != null) {
+          agNWeekdayToPreference[personAgPreferenceToReset.ag.id]![
+              personAgPreferenceToReset.weekday] = "";
+          personAgPreferences.remove(personAgPreferenceToReset);
+        }
+
+        PersonAgPreference? personAgPreference =
+            getPersonAgPreferenceFromAGAndWeekday(ag, weekday);
+
+        if (personAgPreference != null && preferenceString != "") {
+          //set preferenceNumber
+          personAgPreference.preferenceNumber = preference;
+        } else {
+          personAgPreference = PersonAgPreference(
+              id: -1,
+              preferenceNumber: preference,
+              weekday: weekday,
+              person: widget.person,
+              ag: ag);
+          personAgPreferences.add(personAgPreference);
+        }
       }
+      setPersonPreferenceView(ag, weekday, preferenceString);
     });
   }
 
-  void setAGWeekday(AG ag, String weekday) {
-    setState(() {
-      PersonAgPreference? personAgPreference = getPersonAgPreferenceFromAG(ag);
-      if (personAgPreference != null && weekday != "") {
-        personAgPreference.weekday = weekday;
-      }
-    });
+  void setPersonPreferenceView(AG ag, String weekday, String preferenceString) {
+    //set new agNWeekdayToPreference for View
+    if (agNWeekdayToPreference[ag.id] != null) {
+      agNWeekdayToPreference[ag.id]![weekday] = preferenceString;
+    }
   }
 
-  PersonAgPreference? getPersonAgPreferenceFromAG(AG ag) {
+  PersonAgPreference? getPersonAgPreferenceByWeekdayAndPreferenceNumber(
+      String weekday, int preference) {
     for (PersonAgPreference personAgPreference in personAgPreferences) {
-      if (personAgPreference.ag == ag) {
+      if (personAgPreference.weekday == weekday &&
+          personAgPreference.preferenceNumber == preference) {
         return personAgPreference;
       }
     }
     return null;
   }
 
-  String getPreferenceFromAG(AG ag) {
+  PersonAgPreference? getPersonAgPreferenceFromAGAndWeekday(
+      AG ag, String weekday) {
     for (PersonAgPreference personAgPreference in personAgPreferences) {
-      if (personAgPreference.ag == ag) {
-        return personAgPreference.preferenceNumber.toString();
+      if (personAgPreference.ag == ag &&
+          personAgPreference.weekday == weekday) {
+        return personAgPreference;
       }
     }
-    return "";
-  }
-
-  String getWeekdayFromAG(AG ag) {
-    for (PersonAgPreference personAgPreference in personAgPreferences) {
-      if (personAgPreference.ag == ag) {
-        return personAgPreference.weekday;
-      }
-    }
-    return "";
+    return null;
   }
 
   @override
@@ -86,17 +107,34 @@ class _SelectPreferencesState extends State<SelectPreferences> {
     super.initState();
 
     reloadPersonAgPreferences();
+  }
+
+  void reloadPersonAgPreferences() async {
+    personAgPreferences =
+        await widget.persistenceManager.getPersonAgPreferences(widget.person);
+
+    for (AG ag in widget.ags) {
+      for (String weekday in ag.weekdays) {
+        if (agNWeekdayToPreference.keys.contains(ag.id)) {
+          agNWeekdayToPreference[ag.id]!.putIfAbsent(weekday, () => "");
+        } else {
+          agNWeekdayToPreference.putIfAbsent(ag.id, () => {weekday: ""});
+        }
+      }
+    }
+
+    for (PersonAgPreference personAgPreference in personAgPreferences) {
+      if (agNWeekdayToPreference.keys.contains(personAgPreference.ag.id)) {
+        agNWeekdayToPreference[personAgPreference.ag.id]![personAgPreference
+            .weekday] = personAgPreference.preferenceNumber.toString();
+      }
+    }
 
     numberOfPreferencesList = StringUtils.getStringListPlusEmpty(
         StringUtils.copyStringList(StringUtils.intListToStringList(
             IntUtils.intToIntList(settings.numberOfPreferences)
                 .map((int value) => value + 1)
                 .toList())));
-  }
-
-  void reloadPersonAgPreferences() async {
-    personAgPreferences =
-        await widget.persistenceManager.getPersonAgPreferences(widget.person);
     setState(() {});
   }
 
@@ -132,49 +170,39 @@ class _SelectPreferencesState extends State<SelectPreferences> {
                         TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
                   ),
                   Text(
-                    "Präferenz",
+                    "Wochentag",
                     textAlign: TextAlign.center,
                     style:
                         TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
                   ),
                   Text(
-                    "Wochentag",
+                    "Präferenz",
                     textAlign: TextAlign.center,
                     style:
                         TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
                   ),
                 ]),
                 for (AG ag in widget.ags)
-                  TableRow(children: [
-                    Text(ag.toShortString()),
-                    DropdownButton(
-                      value: getPreferenceFromAG(ag),
-                      items: numberOfPreferencesList
-                          .map<DropdownMenuItem<String>>(
-                            (String value) => DropdownMenuItem(
-                                value: value, child: Text(value)),
-                          )
-                          .toList(),
-                      onChanged: (String? value) {
-                        setAGPreference(ag, value!);
-                      },
-                    ),
-                    DropdownButton(
-                      value: getWeekdayFromAG(ag),
-                      items: StringUtils.getStringListPlusEmpty(
-                              StringUtils.copyStringList(
-                                  StringUtils.getIntersectionBetweenTwoLists(
-                                      widget.weekdaysPresent, ag.weekdays)))
-                          .map<DropdownMenuItem<String>>(
-                            (String value) => DropdownMenuItem(
-                                value: value, child: Text(value)),
-                          )
-                          .toList(),
-                      onChanged: (String? value) {
-                        setAGWeekday(ag, value!);
-                      },
-                    ),
-                  ])
+                  for (String weekday in ag.weekdays)
+                    if (agNWeekdayToPreference[ag.id] != null)
+                      TableRow(children: [
+                        Text(ag.toShortString()),
+                        Text(weekday),
+                        DropdownButton(
+                          value: agNWeekdayToPreference[ag.id]![weekday],
+                          items: numberOfPreferencesList
+                              .map<DropdownMenuItem<String>>(
+                                (String value) => DropdownMenuItem(
+                                    value: value, child: Text(value)),
+                              )
+                              .toList(),
+                          onChanged: (String? value) {
+                            if (value != null) {
+                              setAGPreference(ag, weekday, value);
+                            }
+                          },
+                        ),
+                      ])
               ],
             ),
             FloatingActionButton(
